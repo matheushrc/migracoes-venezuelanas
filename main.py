69ba9d0f200ab61a5961275ec78f62932cf08f39
Matheus Henrique <matheus1604.mh@gmail.com>
2025-12-09 17:50:17 -0300

    🔨 refactor(main): simplify model selection and improve logging

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import json
 import os
 import re
-from enum import Enum
 from glob import glob
-from typing import List, Union
+from typing import List, Union, get_args
 
+import logfire
 from dotenv import load_dotenv
 from loguru import logger
+from pydantic_ai.models.google import LatestGoogleModelNames
 
 from src.agents import GoogleAgent
 from src.models import Entrevista, InferenceCreate
 from src.prompts import ENTREVISTA_PROMPT
 from src.tools import get_coordinates
 
+logfire.configure()
+logfire.instrument_pydantic_ai()
 load_dotenv()
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
@@ -21,19 +24,13 @@ if not GOOGLE_API_KEY:
 
 google_agent = GoogleAgent(api_key=GOOGLE_API_KEY)
 
-
-class GeminiModels(str, Enum):
-    # gemini-3-pro-preview
-    GEMINI_3_PRO_PREVIEW = "gemini-3-pro-preview"
-    GEMINI_2_5_PRO = "gemini-2.5-pro"
-    GEMINI_2_5_FLASH = "gemini-2.5-flash"
-    GEMINI_2_5_FLASH_LITE = "gemini-2.5-flash-lite"
-
+# Extract all available model names from the Literal type
+AVAILABLE_GOOGLE_MODELS = get_args(LatestGoogleModelNames)
 
 entrevista_agent = google_agent.create_agent(
     system_prompt=ENTREVISTA_PROMPT,
     output_type=Entrevista,
-    model_name=GeminiModels.GEMINI_2_5_FLASH_LITE,
+    model_name=AVAILABLE_GOOGLE_MODELS[10],  # gemini-3-pro-preview
     model_settings={
         "temperature": 0,
         "max_tokens": 65535,
@@ -48,10 +45,10 @@ USER_PROMPT = """
 """
 
 
-async def main():
+async def process_interviews():
 
     entrevistas = os.listdir("data/entrevistas/clean/")
-    entrevistas = ["entrevista_8.clean.md"]
+    # entrevistas = ["entrevista_8.clean.md"]
     logger.info(f"Total entrevistas to process: {entrevistas}")
 
     os.makedirs("outputs", exist_ok=True)
@@ -84,15 +81,12 @@ async def main():
                 json.dump(response_dict, f, ensure_ascii=False, indent=4)
 
 
-# class GeographicCoordinates(BaseModel):
-#     latitude: float = Field(..., description="Latitude da localidade")
-#     longitude: float = Field(..., description="Longitude da localidade")
-
-
 def insert_coordinates(entrevistas: Union[List[str], str]):
 
     for i, entrevista in enumerate(entrevistas):
-        logger.info(f"Verifying output {i + 1}/{len(entrevistas)}: {entrevista}")
+        logger.info(
+            f"Adicionando coordenadas geográficas na entrevista {i + 1}/{len(entrevistas)}: {entrevista}"
+        )
         with open(entrevista, "r", encoding="utf-8") as f:
             data = json.load(f)
 
@@ -121,7 +115,7 @@ def covert_to_csv(entrevistas: Union[List[str], str]):
     for entrevista in entrevistas:
         with open(entrevista, "r", encoding="utf-8") as f:
             data = json.load(f)
-            
+            logger.info(f"Convertendo entrevista para CSV: {entrevista}")
             id = os.path.basename(entrevista).replace(".json", "")
             deslocamentos = data.get("deslocamento", [])
             for deslocamento in deslocamentos:
@@ -140,11 +134,10 @@ def covert_to_csv(entrevistas: Union[List[str], str]):
 
 
 if __name__ == "__main__":
-    # import asyncio
-
-    # asyncio.run(main())
+    import asyncio
 
     entrevistas = glob("./outputs/*.json")
 
-    # insert_coordinates(entrevistas=entrevistas)
+    asyncio.run(process_interviews())
+    insert_coordinates(entrevistas=entrevistas)
     covert_to_csv(entrevistas=entrevistas)
